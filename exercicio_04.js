144336fbc26e714a902ce8b5f89c6e9d99f40a02
EduardoLopesssss <eduardo_h_lopes@estudante.sesisenai.org.br>
2026-03-19 09:11:38 -0300

    add - Refiz o a Quarta atividade

--- exercicio_04.js
+++ exercicio_04.js
@@ -1,3 +1,9 @@
 function OrdenaçãoValores() { 
+    let numeros = [];
+    for (let i = 0 ; i <4; 1++){
+        numeros.push(Number(prompt("Digite um número"))); 
+    }
+    numeros.sort((a,b) => b - a);
+    alert.log("Ordem decresente:" + numeros);
 }
 
