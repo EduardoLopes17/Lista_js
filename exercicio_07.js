ec7106aa536151ed71187913d10d03ae8f722025
EduardoLopesssss <eduardo_h_lopes@estudante.sesisenai.org.br>
2026-03-24 09:35:55 -0300

    add - Fiz o setima atividade

--- exercicio_07.js
+++ exercicio_07.js
@@ -1,2 +1,21 @@
 function Picoleteria() { 
+    const picoles = [
+        {codigo: "A", sabor: "Chocolate",Preco: 1.50},
+        {codigo: "B", sabor: "Morango",Preco: 2.50},
+        {codigo: "C", sabor: "Creme",Preco: 2.50},
+        {codigo: "D", sabor: "Manga",Preco: 3.20},
+        {codigo: "E", sabor: "Melancia",Preco: 3.40},
+        {codigo: "F", sabor: "Vanília Azul",Preco: 3.00},
+        {codigo: "G", sabor: "Céu Azul",Preco: 3.60},
+        {codígo: "H", sabor: "Brownie",Preco: 4.00},
+        {codigo: "I", sabor: "Hawaiano",Preco: 5.00},
+    ];
+    let codigoPicole = prompt   ("Digite o código para consulta ( A, B, C, D, E, F, G, H, I)");
+    let picole = picoles.find(picole => picole.codigo == codigoPicole);
+    if (picoles) {
+        alert("Dados do Picole: \nSabor: " + picole.sabor + "\nPreço:" + picole.Preco);
+    } else{
+        alert("Digite um codígo para consultar.");
+    }
+
 }
