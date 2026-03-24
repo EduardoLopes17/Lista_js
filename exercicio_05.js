8cb7206105627187efe018f12c7e55491fbc3f45
EduardoLopesssss <eduardo_h_lopes@estudante.sesisenai.org.br>
2026-03-24 07:52:27 -0300

    add - Fiz o quinto atividade

--- exercicio_05.js
+++ exercicio_05.js
@@ -1,2 +1,14 @@
 function Parimpar() { 
+    
+    let numero = 0;
+
+    numero = Number(prompt("Informe um valor"));
+
+    if(numero%2 == 0){
+        numero++;
+    } else {
+        numero++;
+    };
+
+    console.log(numero);
 }
